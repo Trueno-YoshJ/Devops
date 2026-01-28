b9e5443e92e9225b614e5ec0150a1c5c6531144b
Trueno-YoshJ <186725483+Trueno-YoshJ@users.noreply.github.com>
2026-01-29 00:32:17 +0530

    frontend update2

---
+++ Frontend/src/api/config.js
@@ -0,0 +1,5 @@
+// Frontend/src/api/config.js
+export const API_BASE_URL = "http://18.234.113.136:9090";
+export const LOGIN_URL = `${API_BASE_URL}/api/auth/login`;
+export const SIGNUP_URL = `${API_BASE_URL}/api/auth/signup`;
+export const SERVICES_URL = `${API_BASE_URL}/api/services`;
